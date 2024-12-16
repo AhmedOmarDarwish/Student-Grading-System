ee1a79b4cacf2048b95d7a3add273d675cd03f8e
3Mr <3Mr@OMEN>
2024-12-16 04:10:08 +0200

    Add project files.

---
+++ Student Grading System/GradingSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grading_System
+{
+    internal class GradingSystem
+    {
+
+        internal void DisplayGradingInfo(List<Student> students,
+           Func<List<double>,double> calcAverage,
+           Predicate<double> checkIfPassed,
+           Action<Student,double, string> displayData) {
+
+            foreach (Student student in students) {
+                double avargeGrades = calcAverage(student.Grades);
+                bool isStudentPassed = checkIfPassed(avargeGrades);
+                string status = "Failed";
+                if (isStudentPassed) status = "Passed";
+                displayData(student,avargeGrades, status);
+            }
+        
+        }
+    }
+}
